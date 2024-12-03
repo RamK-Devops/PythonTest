52ec7f35d0061122f1cf7d5531ee517f551fb622
Ram K <krams.kota@gmail.com>
2024-12-03 12:13:28 +0000

    new version

---
+++ DemoFunc.py
@@ -0,0 +1,16 @@
+# funcation Demo
+
+# Funcation Declaration
+
+def addtwo(x, y):
+    sumtwo = x+y
+    print("{} {} {} {}".format("sum of",x ,"and",y))
+
+    print(sumtwo)
+
+
+#function call
+a = 5
+b = 3
+
+addtwo(a,b)
---
+++ DemoOops.py
@@ -0,0 +1,11 @@
+class show:
+    print ("you are in show class")
+
+    def getData(self):
+        print("you are in getData")
+
+
+obj1 = show() #create objects
+
+obj1.getData()
+
